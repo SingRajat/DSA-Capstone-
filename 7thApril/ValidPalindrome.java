b32aad88b3f150fd132961c4113210b54b171bae
Rajat Singh <rajatkvongc@gmail.com>
2026-04-08 09:50:21 +0530

    Day 2-code 4

---
+++ 7thApril/ValidPalindrome.java
@@ -0,0 +1,16 @@
+public class ValidPalindrome {
+    static boolean palindrome(String s){
+        int start = 0, end = s.length()-1;
+        while(start<end){
+            if(s.charAt(start)!=s.charAt(end))return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "madam";
+        System.out.println(palindrome(s));
+    }
+}
